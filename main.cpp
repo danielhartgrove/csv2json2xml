15b9ff07a901b03158f777a09ff49ab973c2c5ef
Dan Hartgrove <hartgrovedaniel3@outlook.com>
2024-01-23 11:02:25 +0000

    fix(2json): fixed the category generation in json which prevented more than 2 items from being added, also fixed mysterious ''cout'' segfault

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 // CSV2JSON
 
 #include <iostream>
+#include <stdlib.h>
 #include <string>
 #include <fstream>
 #include <map>
 #include <vector>
 #include <sstream>
+#include <filesystem>
 #include "DataHolder.h"
 
 using namespace std;
@@ -27,6 +29,9 @@ bool extractCSV(string path) {
     string line;
 
     while (getline(input_file, line)) {
+        if(line==""){
+            break;
+        }
         // Create a new vector for each row
         vector<string> row;
 
@@ -34,9 +39,11 @@ bool extractCSV(string path) {
         stringstream ss(line);
         string field;
 
+        
         // Tokenize the line using ',' as the delimiter
         while (getline(ss, field, ',')) {
             row.push_back(field); // add field to the current row
+            
         }
 
         // Add the completed row to the main data
@@ -56,12 +63,12 @@ bool extractJSON(string path) {
     return true;
 }
 
-int convertToCSV(string path, int file_type) {
+bool convertToCSV(string path, int file_type) {
     if (file_type == 2) {
         // convert json to csv
     }
 
-    return 0;
+    return true;
 }
 
 bool convertToJSON(string path, int file_type) {
@@ -72,42 +79,45 @@ bool convertToJSON(string path, int file_type) {
         ofstream outfile(filename);
 
         if (!outfile.is_open()) {
-            cerr << "Error opening file: " << filename << endl;
+            printf("Error opening file: %s\n", filename);
             return 1;
         }
+
         
-        // make a vector of the categories
-        for (int i = 0; i <= DataHolder::getSize(); i++) {
+        for (int i = 0; i < DataHolder::data[0].size(); i++){
             categories.push_back(DataHolder::data[0][i]);
         }
 
+        int count = 0;
+
         outfile << "{\n";
         outfile << "\t\"items\": [\n";
-        for (int i = 1; i < DataHolder::getSize(); i++) { // the 0th line is the names of the categories
+        for (int i = 1; i < DataHolder::getSize(); i++) {
             outfile << "\t\t{\n";
-            for(int j = 0; j < categories.size(); j++){//for each of the categories, print the category name and the value
-                
+
+            // Use a range-based for loop for better readability
+            for (int j = 0; j < categories.size(); j++) {
                 outfile << "\t\t\t\"" << categories[j] << "\": \"" << DataHolder::data[i][j] << "\"";
 
-                // if its at the end of the categories, then we do not need a comma
-                if(j != categories.size() - 1){
+                // Check if it's at the end of the categories, then no need for a comma
+                if (j != categories.size() - 1) {
                     outfile << ",\n";
-                }
-                else{
+                } else {
                     outfile << "\n";
                 }
             }
-            
-            // if its at the end of the elements, then we do not need a comma
-            if(i != DataHolder::getSize() - 1){
+
+            // Check if it's at the end of the elements, then no need for a comma
+            if (i != DataHolder::getSize() - 1) {
                 outfile << "\t\t},\n";
-            }
-            else{
+            } else {
                 outfile << "\t\t}\n";
             }
         }
+
         outfile << "\t]\n}\n";
         outfile.close();
+        printf("Success!\n");
         return true;
     }
     else if (file_type == 3) {
@@ -116,31 +126,22 @@ bool convertToJSON(string path, int file_type) {
     }else{
         return false;
     }
+
+    return true;
 }
 
 int main(int argc, char* argv[]) {
     // check if the correct number of arguments are passed
     if (argc != 3) {
         // if not, then explain the usage of the program
-        cerr << "Usage: " << argv[0] << " <arg1> <arg2>" << endl;
-        return 2;
+        printf("Error: Invalid usage\n");
+        printf("Correct Usage: %s <arg1> <arg2>", argv[0]);
+        exit(2);
     }
 
     string path_name = argv[1];
     string convert_type = argv[2];
-
-
-    ifstream file(path_name);
-
-
-    // check if file exists
-    if (!file.is_open()) {
-        cerr << "Error: cannot open file '" << path_name << "'" << endl;
-        return 1; //error code for unable to open file
-    }
-    file.close(); // make sure to close the file!
-
-
+    
     int file_type = 0;
 
     if (path_name.substr(path_name.find_last_of(".") + 1) == "csv") {
@@ -153,44 +154,50 @@ int main(int argc, char* argv[]) {
         file_type = 3;
     }
     else {
-        cerr << "Error: unknown file type '" << path_name.substr(path_name.find_last_of(".") + 1) << "'" << endl;
-        return 3; //error code for invalid type
+        file_type = 0;
+        /* THIS CODE IS BROKEN IF THE FILE EXTENSION IS INCORRECT */
+        printf("Error: Invalid file type\n");
+        exit(3); //error code for invalid type
+         /* THIS CODE IS BROKEN IF THE FILE EXTENSION IS INCORRECT */
     }
+   
+    ifstream f;
+    f.open(path_name);
 
+    if(!f.is_open()){
+        printf("Error: unable to open file '%s'\n", path_name.c_str());
+        exit(1); //error code for unable to open file
+    }
+    f.close();
 
-    map<string, int> types = { {"csv", 1},{"json", 2},{"xml", 3} };
-
-    switch (types[convert_type]) {
-    case 1:
+    if(convert_type == "csv"){
         if (file_type != 1) {
             convertToCSV(path_name, file_type);
+        }else{
+            printf("Error: file type is already CSV\n");
+            exit(3); //error code for invalid type
         }
-        break;
-    case 2:
+    }
+    else if (convert_type == "json") {
         if (file_type != 2) {
+
             convertToJSON(path_name, file_type);
+        }else{
+            printf("Error: file type is already JSON\n");
+            exit(3); //error code for invalid type
         }
-        break;
-        /*case 3:
-            if(file_type != 3){
-                convertToXML(path_name, file_type);
-            }
-            break;*/
-    default:
-        cerr << "Error: unknown conversion type '" << convert_type << "'" << endl;
-        return 3; //error code for invalid type
+    }
+    else if (convert_type == "xml") {
+        if (file_type != 3) {
+            //convertToXML(path_name, file_type);
+        }else{
+            printf("Error: file type is already XML\n");
+            exit(3); //error code for invalid type
+        }
+    }else{
+        printf("Error: Invalid conversion type\n");
+        exit(3); //error code for invalid type
     }
 
     return 0;
 };
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
